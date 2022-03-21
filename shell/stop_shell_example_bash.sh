e1805a9bfb59c2e0868e00f462a644da605372a5
SangHoon <shlee26@icanman.co.kr>
2022-03-21 11:41:21 +0900

    Create stop_shell_example_bash.sh

---
+++ shell/stop_shell_example_bash.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROC_NAME=fbrecv_f
+
+function wait_stop() {
+  echo "Wait for stop process : $1"
+
+  for (( c=1; c<=20; c++ ))
+  do
+     echo "Check process stopped $c times"
+     pid=`ps -ef|grep -v grep|grep $PROC_NAME|awk '{print $2}'`
+
+     if [ -z $pid ]
+     then
+       echo "Stopped process $PROC_NAME"
+       break
+     else
+       echo "Not yet stopped !!!"
+       kill $pid
+       sleep 3
+     fi
+  done
+}
+
+
+# ---- restart process ----
+wait_stop
+
+echo "Try start $PROC_NAME "
+
+# Execute start up shell script
